91e141cae0721d8deca0692650610601fa0ff61b
Péter Jeszenszky <jeszenszky.peter@inf.unideb.hu>
2025-04-06 17:25:40 +0200

    docs: add Javadoc comments to source files

--- src/main/java/nationalize/Main.java
+++ src/main/java/nationalize/Main.java
@@ -1,5 +1,8 @@
 package nationalize;
 
+/**
+ * CLI for the {@code nationalize} package.
+ */
 public class Main {
 
     public static void main(String[] args) {
--- src/main/java/nationalize/Nationality.java
+++ src/main/java/nationalize/Nationality.java
@@ -4,6 +4,22 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
+/**
+ * Stores the possible countries of origin for a last name.
+ *
+ * @param count the number of rows examined
+ * @param name a last name for which the possible countries of origin are
+ *             predicted
+ * @param countries a list of the most likely countries of origin
+ */
 public record Nationality(long count, String name, @SerializedName("country") List<Country> countries) {
+
+    /**
+     * Associates a probability with a country of origin.
+     *
+     * @param countryId a two-letter country code
+     * @param probability a probability, i.e., a number between 0 and 1
+     */
     public record Country(String countryId, float probability) {}
+
 }
